e14ab92b59f856e35b682dd23c285d7936affdb5
Laura Sofia Rincón <109982216+rinconlaura@users.noreply.github.com>
2022-09-24 16:38:14 -0500

    Update proyecto moles.py

--- proyecto moles.py
+++ proyecto moles.py
@@ -1,31 +1,29 @@
-def molaridad(m, ma, v): #  funcion que calcula la molaridad 
-    c = m/(ma*v)
+def molaridad(m, v): #  funcion que calcula la molaridad 
+    c = m/(v)
     return c
 
 
 
 def presion_gas_ideal(moles, temperatura, volumen): # funcion presion de un gas ideal 
-    r = 8.314
+    r = 0.08205
     return (moles * r * temperatura) / volumen
 
-print("bienvenido si desea calcular la moralidad ingrese 1, si desea calcular la presion de un gas ideal ingrese 2")
+print("bienvenido si desea calcular la moralidad digite 1, si desea calcular la presion de un gas ideal digite 2, si desa calcular el volumen digite 3, si desea calcular los moles digite 4, si desa calcular la temperatura digite 5 ")
 a=input() # lo que entra el usuario 
 
 if a=='1': #el usuario metio 1
-    print('ingrese m')
+    print('ingrese moles')
     m=float(input())
-    print('ingrese ma')
-    ma=float(input())
-    print('ingrese v')
+    print('ingrese volumen (L)')
     v=float(input())
-    m=molaridad(m,ma,v)
+    m=molaridad(m,v)
     print('la molaridad es:', m)
 elif a=='2':
     print('ingrese las moles')
     moles=float(input())
-    print('ingrese temperatura')
+    print('ingrese temperatura (K)')
     temperatura=float(input())
-    print('ingrese volumen ')
+    print('ingrese volumen (L)')
     volumen=float(input())
     p=presion_gas_ideal(moles, temperatura, volumen)    
     print('la presion del gas ideal es: ',p,'atm')
