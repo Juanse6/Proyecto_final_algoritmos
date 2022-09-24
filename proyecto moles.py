9f2c565c8e98e9ec612df264bb70090c994ae266
Marianajar16 <109982206+Marianajar16@users.noreply.github.com>
2022-09-24 16:52:49 -0500

    Update proyecto moles.py
    .....

--- proyecto moles.py
+++ proyecto moles.py
@@ -1,9 +1,6 @@
 def molaridad(m, v): #  funcion que calcula la molaridad 
     c = m/(v)
     return c
-
-
-
 def presion_gas_ideal(moles, temperatura, volumen): # funcion presion de un gas ideal 
     r = 0.08205
     return (moles * r * temperatura) / volumen
@@ -16,6 +13,9 @@ def moles_gas_ideal(volumen, temperatura, presion): # funcion moles de un gas id
 def temperatura_gas_ideal(moles, presion, volumen): # funcion temperatura de un gas ideal 
     r = 0.08205
     return (presion * volumen ) / (moles * r)
+def molalidad(moles, kg): #  funcion que calcula la molaridad 
+    c = moles/(kg)
+    return c
 
 print("bienvenido si desea calcular la moralidad digite 1, si desea calcular la presion de un gas ideal digite 2, si desa calcular el volumen digite 3, si desea calcular los moles digite 4, si desa calcular la temperatura digite 5 ")
 a=input() # lo que entra el usuario 
@@ -63,6 +63,12 @@ elif a=='5':
     presion=float(input())
     t=temperatura_gas_ideal(moles, presion,volumen)    
     print('La temperatura es: ',t,'K')
-    
+ elif a=='6':
+    print('ingrese moles de soluto')
+    moles=float(input())
+    print('ingrese masa del solvente')
+    kg=float(input())
+    mola=molalidad(moles,kg)
+    print('La molalidad es: ', mola) 
    
     
